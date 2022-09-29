b0200bd1fa0c9524e6ea41742e91d9c8e60d1e16
Kevin Mulvey <kmulvey@linux.com>
2022-09-29 17:05:43 -0600

    RandomString

---
+++ random_string.go
@@ -0,0 +1,20 @@
+package goutils
+
+import (
+	"math/rand"
+	"time"
+)
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func RandomString(n int) string {
+	s := make([]rune, n)
+	for i := range s {
+		s[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(s)
+}
