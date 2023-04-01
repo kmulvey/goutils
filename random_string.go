44c7d903206612c69744fac984090b491cace862
Kevin Mulvey <kmulvey@linux.com>
2023-04-01 09:24:56 -0600

    rand lint errors
    Signed-off-by: Kevin Mulvey <kmulvey@linux.com>

--- random_string.go
+++ random_string.go
@@ -6,15 +6,17 @@ import (
 )
 
 var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+var randomSource *rand.Rand
 
 func init() {
-	rand.Seed(time.Now().UnixNano())
+	//nolint:gosec
+	randomSource = rand.New(rand.NewSource(time.Now().UnixNano()))
 }
 
 func RandomString(n int) string {
 	s := make([]rune, n)
 	for i := range s {
-		s[i] = letters[rand.Intn(len(letters))]
+		s[i] = letters[randomSource.Intn(len(letters))]
 	}
 	return string(s)
 }
