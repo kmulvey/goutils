a9a165ce7d08fa478e894018c8e57e4e06dd8ef1
Kevin Mulvey <kmulvey@linux.com>
2024-05-16 12:23:00 -0600

    avoid data races in RandomString

--- random_string.go
+++ random_string.go
@@ -5,18 +5,15 @@ import (
 	"time"
 )
 
-var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
-var randomSource *rand.Rand
-
-func init() {
-	//nolint:gosec
-	randomSource = rand.New(rand.NewSource(time.Now().UnixNano()))
-}
-
+// RandomString returns a cases sensitive random string of the given length. The randonSource and
+// character array are in the function to avoid data races. At this time its overkill to store them
+// in a type but this can be done in the future if necessary.
 func RandomString(n int) string {
+	//nolint:gosec
+	var randomSource = rand.New(rand.NewSource(time.Now().UnixNano()))
 	s := make([]rune, n)
 	for i := range s {
-		s[i] = letters[randomSource.Intn(len(letters))]
+		s[i] = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")[randomSource.Intn(52)]
 	}
 	return string(s)
 }
--- random_string_test.go
+++ random_string_test.go
@@ -1,6 +1,7 @@
 package goutils
 
 import (
+	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -16,4 +17,22 @@ func TestRandomString(t *testing.T) {
 	assert.Equal(t, 10, len(a))
 
 	assert.NotEqual(t, a, b)
+}
+
+// make sure there is no data race
+func TestRandomStringParallel(t *testing.T) {
+	t.Parallel()
+
+	var wg sync.WaitGroup
+	for i := 0; i < 50; i++ {
+		wg.Add(1)
+		go func(wg *sync.WaitGroup) {
+			for i := 0; i < 1000; i++ {
+				var a = RandomString(10)
+				assert.Equal(t, 10, len(a))
+			}
+			wg.Done()
+		}(&wg)
+	}
+	wg.Wait()
 }
